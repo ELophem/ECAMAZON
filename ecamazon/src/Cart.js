3c1abf735363868085f75eeecac6e638ba674049
ELophem <18072@ecam.be>
2023-11-28 13:29:39 +0100

    OUPS

--- ecamazon/src/Cart.js
+++ ecamazon/src/Cart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as  Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { useShopContext } from './shop-context';
 import './Product.css';
 import './Cart.css';
